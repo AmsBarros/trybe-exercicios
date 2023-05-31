9820862b2cf1b7cc355888068f6279c21e336814
AmsBarros <amandabarros02@msn.com>
2023-05-31 13:26:21 -0300

    resolução exerc 02

--- fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-01-primeiros-passos-javascript/exercicio-02.js
+++ fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-01-primeiros-passos-javascript/exercicio-02.js
@@ -1,2 +1,8 @@
-let primeiroNum = 89;
-let segundoNum = 20;
+let primeiroNum = 10;
+let segundoNum = 49;
+
+if (primeiroNum > segundoNum) {
+    console.log(primeiroNum + " é o maior número");
+} else {
+    console.log(segundoNum + " é o maior número");
+}
