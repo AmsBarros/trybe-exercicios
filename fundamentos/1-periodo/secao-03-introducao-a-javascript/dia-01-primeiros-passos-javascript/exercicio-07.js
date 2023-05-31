cd17defe49c80d01dd47cdfd7dd4fbcd093611e3
AmsBarros <amandabarros02@msn.com>
2023-05-31 18:54:28 -0300

    Desacreditada, porḿém, exercicio 7 feito e funcionando

--- fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-01-primeiros-passos-javascript/exercicio-07.js
+++ fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-01-primeiros-passos-javascript/exercicio-07.js
@@ -1,7 +1,8 @@
-let salarioBruto = 6000;
+let salarioBruto = 3000;
 let descontoINSS;
 let descontoIR;
 let salarioBase;
+let salarioLiquido;
 
 if (salarioBruto <= 1556.94) {
     descontoINSS = (salarioBruto * 8) / 100;
@@ -17,7 +18,25 @@ if (salarioBruto <= 1556.94) {
     salarioBase = salarioBruto - descontoINSS; 
 }
 
+if (salarioBase > 4664.68) {
+    descontoIR = (salarioBase * 27.5) / 100 - 869,36;
+    salarioLiquido = salarioBase - descontoIR;
+} else if (salarioBase <= 4664.68 && salarioBase >= 3751.06) {
+    descontoIR = (salarioBase * 22.5) / 100 - 636.13;
+    salarioLiquido = salarioBase - descontoIR;
+} else if (salarioBase <= 3751.05 && salarioBase >= 2826.66) {
+    descontoIR = (salarioBase * 15) / 100 - 354.80;
+    salarioLiquido = salarioBase - descontoIR;
+} else if (salarioBase <= 2826.65 && salarioBase >= 1903.99) {
+    descontoIR = (salarioBase * 7.5) / 100 - 142.80;
+    salarioLiquido = salarioBase - descontoIR;
+} else {
+    descontoIR = 0;
+    salarioLiquido = salarioBase - descontoIR;
+}
+
 
 console.log(salarioBruto);
 console.log(descontoINSS);
 console.log(salarioBase);
+console.log(salarioLiquido)
