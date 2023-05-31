a1d41dfdb9ad3d19741a8a9d051fcfa67a6bb680
AmsBarros <amandabarros02@msn.com>
2023-05-31 15:42:20 -0300

    exercicio 06

---
+++ fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-01-primeiros-passos-javascript/exercicio-06.js
@@ -0,0 +1,26 @@
+let num1 = 25;
+let num2 = 9;
+let num3 = 15;
+
+let numPar = false;
+
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+    numPar = true;
+}
+
+console.log(numPar);
+
+/*GABARITO
+
+const num1 = 1;
+const num2 = 3;
+const num3 = 5;
+
+let isEven = false;
+
+if ((num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0)) {
+  isEven = true;
+};
+
+console.log(isEven);
+*/
