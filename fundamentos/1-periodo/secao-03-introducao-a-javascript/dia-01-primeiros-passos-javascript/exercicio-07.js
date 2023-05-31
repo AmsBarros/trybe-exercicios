eb6ea7b8ee9512d79324305effecff2ac432a10e
AmsBarros <amandabarros02@msn.com>
2023-05-31 19:13:32 -0300

    Add comentarios ao exerc 07

--- fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-01-primeiros-passos-javascript/exercicio-07.js
+++ fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-01-primeiros-passos-javascript/exercicio-07.js
@@ -18,6 +18,7 @@ if (salarioBruto <= 1556.94) {
     salarioBase = salarioBruto - descontoINSS; 
 }
 
+
 if (salarioBase > 4664.68) {
     descontoIR = (salarioBase * 27.5) / 100 - 869,36;
     salarioLiquido = salarioBase - descontoIR;
@@ -40,3 +41,37 @@ console.log(salarioBruto);
 console.log(descontoINSS);
 console.log(salarioBase);
 console.log(salarioLiquido)
+
+/*GABARITO
+
+let aliquotINSS;
+let aliquotIR;
+
+let grossSalary = 2000;
+
+if (grossSalary <= 1556.94) {
+  aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+  aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+  aliquotINSS = grossSalary * 0.11;
+} else {
+  aliquotINSS = 570.88;
+};
+
+let baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+  aliquotIR = (baseSalary * 0.075) - 142.80;
+} else if (baseSalary <= 3751.05) {
+  aliquotIR = (baseSalary * 0.15) - 354.80;
+} else if (baseSalary <= 4664.68) {
+  aliquotIR = (baseSalary * 0.225) - 636.13;
+} else {
+  aliquotIR = (baseSalary * 0.275) - 869.36;
+};
+
+console.log("Salário: R$" + (baseSalary - aliquotIR));
+*/
