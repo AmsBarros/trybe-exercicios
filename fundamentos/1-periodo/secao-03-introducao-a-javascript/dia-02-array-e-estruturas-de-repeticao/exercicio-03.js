0678a6ebd1d17afef09770e2c0f7cfab871d1bcd
AmsBarros <amandabarros02@msn.com>
2023-06-01 19:14:31 -0300

    Exercicio 03

---
+++ fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-02-array-e-estruturas-de-repeticao/exercicio-03.js
@@ -0,0 +1,16 @@
+let word = 'tryber';
+let letters = word.split('');
+letters.reverse();
+let newWord = letters.join('')
+console.log(letters);
+console.log(newWord);
+
+/* GABARITO
+
+let word = 'tryber';
+let reverseWord = '';
+
+reverseWord = word.split('').reverse().join('');
+
+console.log(reverseWord);
+*/
