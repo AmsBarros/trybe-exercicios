d025f7b675759443320a1f33aa41b56b14c006b1
AmsBarros <amandabarros02@msn.com>
2023-06-02 10:10:21 -0300

    Exercicio 06

--- fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-02-array-e-estruturas-de-repeticao/exercicio-06.js
+++ fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-02-array-e-estruturas-de-repeticao/exercicio-06.js
@@ -1,3 +1,22 @@
+const n = 5;
+
+for (let i = 1; i <= n; i++) {
+  let linha = '';
+
+  // Adiciona espaços em branco antes dos asteriscos
+  for (let j = 1; j <= n - i; j++) {
+    linha += ' ';
+  }
+
+  // Adiciona os asteriscos
+  for (let k = 1; k <= i; k++) {
+    linha += '*';
+  }
+
+  console.log(linha);
+}
+
+/*GABARITO
 let n = 5;
 let symbol = '*';
 let inputLine = '';
@@ -15,3 +34,4 @@ for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
   inputLine = '';
   inputPosition -= 1;
 };
+*/
