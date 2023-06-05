5070a69a165de2aa76b07d52d3585abae963389a
AmsBarros <amandabarros02@msn.com>
2023-06-05 18:21:15 -0300

    Exercicio 01

--- fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-04-objetos/pratica-01.js
+++ fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-04-objetos/pratica-01.js
@@ -3,13 +3,29 @@ const reader = {
     lastName: 'Pessoa',
     age: 21,
     favoriteBooks: [
-      {
-        title: 'O Senhor dos Anéis - a Sociedade do Anel',
-        author: 'J. R. R. Tolkien',
-        publisher: 'Martins Fontes',
-      },
+        {
+            title: 'O Senhor dos Anéis - a Sociedade do Anel',
+            author: 'J. R. R. Tolkien',
+            publisher: 'Martins Fontes',
+        },
     ],
-  };
+};
 
 
-  console.log(`O livro favorito de ${reader.name} ${reader.lastName} se chama '${Object.values(reader.favoriteBooks[0])}'`);
+console.log(`O livro favorito de ${reader.name} ${reader.lastName} se chama '${Object.values(reader.favoriteBooks[0])}'.`);
+
+let newBook = [
+    {
+        title: 'Harry Potter e o Prisioneiro de Azkaban',
+        author: 'JK Rowling',
+        publisher: 'Rocco',
+    }
+];
+
+reader.favoriteBooks.push(newBook);
+
+if (reader['favoriteBooks'].length == 1) {
+    console.log("Julia tem 1 livro favorito.");
+} else {
+    console.log(`Julia tem ${reader['favoriteBooks'].length} livros favoritos.`)
+}
