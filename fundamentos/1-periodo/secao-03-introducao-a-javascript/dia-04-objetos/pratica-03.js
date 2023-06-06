37295ea01002288d7a5dffab3f35971152a2c842
AmsBarros <amandabarros02@msn.com>
2023-06-06 09:41:30 -0300

    Começando exercício 03

---
+++ fundamentos/1-periodo/secao-03-introducao-a-javascript/dia-04-objetos/pratica-03.js
@@ -0,0 +1,56 @@
+const school = {
+    lessons: [
+      {
+        course: 'Python',
+        students: 20,
+        professor: 'Carlos Patrício',
+        shift: 'Manhã',
+      },
+      {
+        course: 'Kotlin',
+        students: 10,
+        professor: 'Gabriel Oliva',
+        shift: 'Noite',
+      },
+      {
+        course: 'JavaScript',
+        students: 738,
+        professor: 'Gustavo Caetano',
+        shift: 'Tarde',
+      },
+      {
+        course: 'MongoDB',
+        students: 50,
+        shift: 'Noite',
+      },
+    ]
+  };
+
+  function getIndex(obj, index) {
+    return Object.values(obj)[index];
+  }
+
+  console.log(getIndex(school, 0));
+
+  function sumStudents(object) {
+    let sumStudents = 0;
+    for (let index = 0; index < object.lessons.length; index += 1) {
+        sumStudents += object.lessons[index].students;
+    }
+    return sumStudents;
+  }
+
+  console.log(sumStudents(school));
+
+  //AINDA NÃO FUNCIONA. TERMINAR DEPOIS
+  function isThere(object, key) {
+    for (let index = 0; index < object.lessons.length; index += 1) {
+        if (Object.keys(object.lessons[index]) !== 'undefined') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+  }
+
+  console.log(isThere(school,school.lessons.professor));
