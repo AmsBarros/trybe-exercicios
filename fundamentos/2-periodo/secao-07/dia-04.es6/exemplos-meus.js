8398f81121c6911e4f4553881d63541df677b7cc
AmsBarros <amandabarros02@msn.com>
2023-08-06 11:13:08 -0300

    Ajeitando pastas

--- fundamentos/2-periodo/secao-07/dia-04.es6/exemplos-meus.js
+++ fundamentos/2-periodo/secao-07/dia-04.es6/exemplos-meus.js
@@ -82,3 +82,31 @@ const pessoasComHabilidades = pessoas.map((pessoa) => ({
 }));
 
 console.log(pessoasComHabilidades);
+
+
+
+const cart = [
+  {
+    name: "laptop",
+    price: 1000,
+    quantity: 2,
+    id: 3,
+  },
+  {
+    name: "desktop",
+    price: 1500,
+    quantity: 1,
+    id: 2,
+  },
+  {
+    name: "phone",
+    price: 500,
+    quantity: 3,
+    id: 1,
+  },
+];
+
+const valorInicial = 0;
+const totalCart = cart.reduce((accumulator, product) => accumulator + product.price * product.quantity, valorInicial);
+
+console.log(totalCart);
