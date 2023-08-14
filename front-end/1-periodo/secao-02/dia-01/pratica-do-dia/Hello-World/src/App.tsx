35701dadbc114f3beba5a678ee6de153c2bf1638
AmsBarros <amandabarros02@msn.com>
2023-08-14 13:59:22 -0300

    Exercício do dia

---
+++ front-end/1-periodo/secao-02/dia-01/pratica-do-dia/Hello-World/src/App.tsx
@@ -0,0 +1,13 @@
+import HelloWorld from './components/hello-world'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <HelloWorld />
+    </div>
+  )
+}
+
+export default App
