6df384b47208dc54261c304191a1e7040417a8c6
AmsBarros <amandabarros02@msn.com>
2023-09-25 11:14:16 -0300

    Acrescentando praticas conteudo

--- front-end/2-periodo/secao-06/dia-02-redux-com-react/pratica-conteudo/redux-com-react/src/App.tsx
+++ front-end/2-periodo/secao-06/dia-02-redux-com-react/pratica-conteudo/redux-com-react/src/App.tsx
@@ -1,21 +1,30 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { actionCreator } from './redux/actions';
+import { actionCreator, clickCounter } from './redux/actions';
 import './App.css';
 
 type RootState = {
   count: number;
+  clicks: number;
 };
 
 function App() {
   const count = useSelector((state: RootState) => state.count);
+  const clicks = useSelector((state: RootState) => state.clicks);
   const dispatch = useDispatch();
+
+  function handleClick(count = 1) {
+    dispatch(actionCreator(count));
+    dispatch(clickCounter());
+  }
   
   return (
     <>
       <h1>Contador</h1>
       <h2>{ count }</h2>
-      <button onClick={ () => dispatch(actionCreator()) }>Adiciona 1</button>
-      <button onClick={ () => dispatch(actionCreator(5)) }>Adiciona 5</button>
+      <h1>Clicks</h1>
+      <h2>{ clicks }</h2>
+      <button onClick={ () => handleClick() }>Adiciona 1</button>
+      <button onClick={ () => handleClick(5) }>Adiciona 5</button>
     </>
   )
 }
